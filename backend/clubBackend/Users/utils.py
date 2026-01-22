2fc4e063c69ae075023bbfa321a055e34923623e
Sandeep Khadka <sandeepkhadka527@gmail.com>
2026-01-22 23:39:06 +0545

    corrected

--- backend/clubBackend/Users/utils.py
+++ backend/clubBackend/Users/utils.py
@@ -1,12 +1,3 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(password):
-    return generate_password_hash(password)
-
-def verify_password(password, hashed):
-    return check_password_hash(hashed, password)
-
-
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
