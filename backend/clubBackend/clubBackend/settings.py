db317fc466208b493ab793d3e7afdfd1e8edacd9
Rojin Dhami <rojindhami98@gmail.com>
2026-02-21 22:46:04 +0545

    Modify email backend settings based on environment
    Update email backend settings to use console backend on Render.

--- backend/clubBackend/clubBackend/settings.py
+++ backend/clubBackend/clubBackend/settings.py
@@ -182,12 +182,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
 # Email Settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+if os.environ.get('RENDER'):  # only on Render
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
 
 EMAIL_USE_SSL = False
 
